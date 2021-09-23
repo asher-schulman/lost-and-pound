abfd8aa66f4fdfa86537c3b77d4c546b8b36109a
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-09-23 16:31:06 -0400

    footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Navbar, Container} from 'react-bootstrap'
-import { LogoLinkedin } from 'react-ionicons'
+import { LogoLinkedin, LogoGithub, Link } from 'react-ionicons'
 
 function Footer() {
     return (
@@ -11,22 +11,35 @@ function Footer() {
                         Software Engineers
                     </ul>
                     <li>Asher Schulman
-                    <LogoLinkedin
+                    <a href="https://github.com/asher-schulman">
+                    <LogoGithub
                       color={'#00000'}
-                      title="LinkedIn"
+                      title="Github"
                       height="25px"
                       width="25px"
                     />
-                    </li>
-                    <li>Cavell Wong
-                    <LogoLinkedin
-                      color={'#00000'}
-                      title="LinkedIn"
-                      height="25px"
-                      width="25px"
-                    />
+                    </a>
 
                     </li>
+                    <li>Cavell Wong
+                    <a href= "https://www.linkedin.com/in/cavellw/">
+                    <LogoLinkedin
+                      color={'#00000'}
+                      title="LinkedIn"
+                      height="25px"
+                      width="25px"/>
+                    </a>
+
+                    <a href="https://github.com/cavellerson">
+                    <LogoGithub
+                      color={'#00000'}
+                      title="Github"
+                      height="25px"
+                      width="25px"
+                    />
+                    </a>
+                    </li>
+
                 </div>
 
                 <div>
@@ -34,20 +47,33 @@ function Footer() {
                         UX Designers
                     </ul>
                     <li>Andrea Murray
+                    <a href="">
                     <LogoLinkedin
                       color={'#00000'}
                       title="LinkedIn"
                       height="25px"
                       width="25px"
                     />
+                    </a>
                     </li>
                     <li>Sara Reid
+                    <a href="https://www.linkedin.com/in/saraeletareid/">
                     <LogoLinkedin
                       color={'#00000'}
                       title="LinkedIn"
                       height="25px"
                       width="25px"
                     />
+                    </a>
+
+                    <a href="https://www.saraeletaux.com/">
+                    <Link
+                      color={'#00000'}
+                      title="Portfolio"
+                      height="25px"
+                      width="25px"
+                    />
+                    </a>
                     </li>
                 </div>
             </Container>

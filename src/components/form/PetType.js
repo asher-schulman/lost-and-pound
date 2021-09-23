bcf5013a15794a23c6f6c3eb977fd0202c538304
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-09-23 03:47:30 -0400

    made the forms for pet's attributes according to dog/cat

--- src/components/form/PetType.js
+++ src/components/form/PetType.js
@@ -11,9 +11,9 @@ const PetType = ({nextStep, handleChange, values}) => {
     return (
         <Container>
             <Form>
-                <Button value='dog' onClick={handleChange('pet_type')}>Dog</Button>
-                <Button value='cat' onClick={handleChange('pet_type')}>Cat</Button>
-                <Button onClick={Continue}>Next</Button>
+                <Button className='circleButton m-2' variant="outline-dark" size="lg" value='dog' onClick={handleChange('pet_type')}>Dog</Button>
+                <Button className='circleButton m-2' variant="outline-dark" size="lg" value='cat' onClick={handleChange('pet_type')}>Cat</Button>
+                <Button className='circleButton m-2' variant="outline-dark" size="lg" onClick={Continue}>Next</Button>
             </Form>
         </Container>
     )
--- src/components/form/Preface.js
+++ src/components/form/Preface.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import {Form, Container, Button} from 'react-bootstrap'
 
-function Preface() {
+const Preface = ({nextStep}) => {
+
+    const Continue = (e) => {
+        e.preventDefault();
+        nextStep();
+    }
+
     return (
         <div>
             <h1>PREFACE</h1>
+            <h2>We've got your back with neighbors and fellow pet lovers who can help out.</h2>
+
+            <Button onClick={Continue} className='circleButton m-2' variant="outline-dark" size="lg">Continue</Button>
         </div>
     )
 }
+
 
 export default Preface

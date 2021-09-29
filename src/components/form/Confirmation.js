b2bbef1f206876cadaeff1bd82e1a898aa421737
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-09-28 22:36:04 -0400

    made it where the text summarization is relatively cleaned depending on puppy or kitten etc...

--- src/components/form/Confirmation.js
+++ src/components/form/Confirmation.js
@@ -2,6 +2,56 @@ import React from 'react'
 import {Container, Button} from 'react-bootstrap'
 
 const Confirmation = ({nextStep, prevStep, handleChange, values}) => {
+
+    const dogSizes = {
+        0: "X-Small",
+        1: "Small",
+        2: "Medium",
+        3: "Large",
+        4: "X-Large",
+        5: "XX-Large"
+    }
+
+    const catSizes = {
+        0: "Very Thin",
+        1: "Thin",
+        2: "Ideal",
+        3: "Overweight",
+        4: "Obese"
+    }
+
+    const dogAges = {
+        0: "Puppy",
+        1: "Adult",
+        2: "Senior"
+    }
+
+    const catAges = {
+        0: "Kitten",
+        1: "Adult",
+        2: "Senior"
+    }
+
+
+    let petInfo= {
+        age: '',
+        size: ''
+    }
+
+
+    if (values["pet_type"] === "dog") {
+        petInfo["age"] = dogAges[parseInt(values["age"])]
+        petInfo["size"] = dogSizes[parseInt(values["pet_size"])]
+    } else if (values["pet_type"] === "cat"){
+        petInfo["age"] = catAges[parseInt(values["age"])]
+        petInfo["size"] = catSizes[parseInt(values["pet_size"])]
+    } else {
+
+    }
+
+
+
+
     return (
         <Container>
             <h1>Please insure the information below is correct</h1>
@@ -14,8 +64,8 @@ const Confirmation = ({nextStep, prevStep, handleChange, values}) => {
                 <li> Coat color: {values.coat_color} </li>
                 <li> Eye color: {values.eye_color} </li>
                 <li> Sex: {values.sex} </li>
-                <li> Size {values.pet_size} </li>
-                <li> Age: {values.age} </li>
+                <li> Size: {petInfo.size}</li>
+                <li> Age: {petInfo.age} </li>
                 <li> Misc: {values.other_info} </li>
                 <li> Date Lost: {values.date_lost} </li>
                 <li> Time Lost: {values.time_lost} </li>
--- src/components/form/Success.js
+++ src/components/form/Success.js
@@ -1,21 +1,67 @@
-import React from 'react'
+import {React, useState} from 'react'
 import {Button} from 'react-bootstrap'
 const Success = ({nextStep, prevStep, handleChange, values, renderFalse}) => {
-    // <img src="https://i.imgur.com/yQLDyiR.jpg"></img>
-    // const postToAPI = () => {
-    //     renderFalse();
-    // }
+
+    const dogSizes = {
+        0: "X-Small",
+        1: "small",
+        2: "medium",
+        3: "large",
+        4: "X-Large",
+        5: "XX-Large"
+    }
+
+    const catSizes = {
+        0: "very Thin",
+        1: "thin",
+        2: "ideal",
+        3: "overweight",
+        4: "obese"
+    }
+
+    const dogAges = {
+        0: "puppy",
+        1: "adult",
+        2: "senior"
+    }
+
+    const catAges = {
+        0: "kitten",
+        1: "adult",
+        2: "senior"
+    }
+
+
+    let petInfo= {
+        age: '',
+        size: ''
+    }
+
+
+    if (values["pet_type"] === "dog") {
+        petInfo["age"] = dogAges[parseInt(values["age"])]
+        petInfo["size"] = dogSizes[parseInt(values["pet_size"])]
+    } else if (values["pet_type"] === "cat"){
+        petInfo["age"] = catAges[parseInt(values["age"])]
+        petInfo["size"] = catSizes[parseInt(values["pet_size"])]
+    } else {
+
+    }
+
+    // const [dateString, setDateString] = useState(values.date_lost)
+    //
+    //
+    // values.date_lost.toString();
+    // setDateString()
 
     return (
         <div>
             <p>Your listing is published! We hope your pet is home soon.
             </p>
             <p>Take a screenshot of your listing to easily share on social media.</p>
-            <p>{values.pet_name} is a {values.pet_size}
-            {values.sex}
-                {values.pet_type}
+            <p>{values.pet_name} is a {petInfo.size} {values.sex === "M" ? <span>male</span> : <span> female </span> } {petInfo.age === "Puppy" || "Kitten" ? <span> {petInfo.age} </span> : <span>{petInfo.age} {values.pet_type}</span>}. {values.sex === "M" ? <span> He </span> : <span> She </span>} was lost at around 
             </p>
-            <Button onClick={nextStep}>Confirm</Button>
+            <Button onClick={nextStep}>See Listings</Button>
         </div>
     )
 }
